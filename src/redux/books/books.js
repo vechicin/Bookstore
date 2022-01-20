1c76268873d870a6b0aeb3c5b22baf194876bd6b
Santiago Velosa <santiagovelosa@gmail.com>
2022-01-19 22:39:33 -0500

    Initialize API communication

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,9 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const FETCH_BOOK = 'bookStore/books/FETCH_BOOK';
 const initialState = [];
+
+const apiUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/QKoyZOzmkUQhnurV30Gv/books';
 
 const addBook = (payload) => ({
   type: ADD_BOOK,
